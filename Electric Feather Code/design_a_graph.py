7c0eef0b6feaa35b16a04442b4e9d2c55ca69e1d
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-01-12 03:14:43 +0000

    create graph_function_paiger

--- Electric Feather Code/design_a_graph.py
+++ Electric Feather Code/design_a_graph.py
@@ -10,7 +10,7 @@ pi = math.pi;
 
 i = cmath.sqrt(-1);
 e = i**2;
-list = [];
+originallist = [];
 array = {(1/2), 1.00, 2.00, 3.00, e, i};
 
 def graph_function_zipper(list, array):
@@ -23,3 +23,20 @@ def graph_function_zipper(list, array):
         print("No results found.");
 
 graph_function_zipper([], array);
+
+list2 = array;
+array_two = {2**(0/1), 1.0, 1.9, 9.8, 8.29, 9.84, "math.log2(-1))"};
+list3 = list(zip(list2, array_two));
+# Erroneous Logic based on the definition of list using curly brackets, which are typically used to create dictionaries. list4 = [i * array_two.index(9.84) in array_two];
+
+def graph_function_paiger(list2, array_two, list3):
+    if array_two == list2:
+        return list2; return list3; print(list3);
+    else:
+        print(list3[0]);
+    
+    if any(item in list3 for item in array):
+        for i in range(len(list3)):
+            print(i, list3);
+
+graph_function_paiger([1], ["I love you"], list3)
